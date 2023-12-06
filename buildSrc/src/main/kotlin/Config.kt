a22cb50ddaf71d9b1f48e65a465f3b35265727a4
lkw1120 <lkwkang@gmail.com>
2023-12-06 20:00:31 +0900

    Update build.gradle

---
+++ buildSrc/src/main/kotlin/Config.kt
@@ -0,0 +1,15 @@
+object Config {
+
+    const val APP_NAME = "Pokedex"
+
+    const val COMPILE_SDK = 34
+
+    const val MINIMUM_SDK = 26
+
+    const val TARGET_SDK = 34
+
+    const val VERSION_CODE = 1
+
+    const val VERSION_NAME = "1.0.0"
+
+}
