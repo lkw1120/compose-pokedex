c962f1b8a36d577de04558a9a55e5deb81ef7c00
lkw1120 <lkwkang@gmail.com>
2024-05-26 17:42:57 +0900

    Update API fetch flow

--- core/network/src/main/java/com/lkw1120/pokedex/core/network/RemoteDataSource.kt
+++ core/network/src/main/java/com/lkw1120/pokedex/core/network/RemoteDataSource.kt
@@ -2,27 +2,34 @@ package com.lkw1120.pokedex.core.network
 
 import com.lkw1120.pokedex.core.network.response.PokeDetailResp
 import com.lkw1120.pokedex.core.network.response.PokeListResp
-import retrofit2.Response
 import javax.inject.Inject
 
 interface RemoteDataSource {
 
-    suspend fun getPokeList(offset: Int): Response<PokeListResp>
+    suspend fun getPokeList(offset: Int): PokeListResp
 
-    suspend fun getPokeDetail(name: String): Response<PokeDetailResp>
+    suspend fun getPokeDetail(name: String): PokeDetailResp
 }
 
 class RemoteDataSourceImpl @Inject constructor(
     private val apiService: ApiService
 ) : RemoteDataSource {
 
-    override suspend fun getPokeList(offset: Int): Response<PokeListResp> {
-        return apiService.getPokeList(offset = offset)
+    override suspend fun getPokeList(offset: Int): PokeListResp {
+        val response = apiService.getPokeList(offset)
+        if(response.isSuccessful) {
+            return response.body()?:throw Exception()
+        }
+        else throw Exception(response.errorBody().toString())
     }
 
 
-    override suspend fun getPokeDetail(name: String): Response<PokeDetailResp> {
-        return apiService.getPokeDetail(name)
+    override suspend fun getPokeDetail(name: String): PokeDetailResp {
+        val response = apiService.getPokeDetail(name)
+        if(response.isSuccessful) {
+            return response.body()?:throw Exception()
+        }
+        else throw Exception(response.errorBody().toString())
     }
 
 }
